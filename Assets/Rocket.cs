796e6c4e80447560021a4c4b51178f050e88689e
Jake Stauch <jake@neuro.plus>
2018-09-05 18:39:16 -0400

    Motion and Sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField]float thrustForce;
+    [SerializeField]float rotationSpeed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+		
+	}
+
+
+    private void Thrust()
+    {
+        float thrustThisFrame = thrustForce * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+            if (!audioSource.isPlaying) // so it doesn't layer
+            {
+                audioSource.Play();
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics control 
+    }
+}
+ 
